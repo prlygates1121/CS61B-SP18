c0bcc6735b57a8be4bc99aa37555be682aae7844
prlygates1121 <3095901180@qq.com>
2024-07-04 14:28:28 +0800

    HW1 3rd try

--- hw1/synthesizer/AbstractBoundedQueue.java
+++ hw1/synthesizer/AbstractBoundedQueue.java
@@ -14,22 +14,4 @@ public abstract class AbstractBoundedQueue<T> implements BoundedQueue<T> {
         return fillCount;
     }
 
-    @Override
-    public boolean isEmpty() {
-        return fillCount == 0;
-    }
-
-    @Override
-    public boolean isFull() {
-        return fillCount == capacity;
-    }
-
-    @Override
-    public abstract T peek();
-
-    @Override
-    public abstract T dequeue();
-
-    @Override
-    public abstract void enqueue(T x);
 }
